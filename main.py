c3940e57aa50a1800fa81f607202c953fa903410
Jedai Saboteur <paradox.inversion@gmail.com>
2022-08-05 01:34:56 -0700

    Part 8

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import copy
+import traceback
+
 import tcod
 import color
 from engine import Engine
@@ -14,6 +16,7 @@ def main() -> None:
     room_min_size = 6
     max_rooms = 30
     max_monsters_per_room = 2
+    max_items_per_room = 2
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
@@ -28,6 +31,7 @@ def main() -> None:
        map_width=map_width,
        map_height=map_height,
        max_monsters_per_room=max_monsters_per_room,
+       max_items_per_room=max_items_per_room,
        engine=engine,
    )
     engine.update_fov()
@@ -46,7 +50,14 @@ def main() -> None:
             root_console.clear()
             engine.event_handler.on_render(console=root_console)
             context.present(root_console)
-            engine.event_handler.handle_events(context)
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+            except Exception:  # Handle exceptions in game.
+                traceback.print_exc()  # Print error to stderr.
+                # Then print the error to the message log.
+                engine.message_log.add_message(traceback.format_exc(), color.error)
 
 
 
