10714758246437bdf1ebc0f9a987fc31417b0ed1
Jedai Saboteur <paradox.inversion@gmail.com>
2022-08-04 21:06:10 -0700

    Part 6

--- engine.py
+++ engine.py
@@ -1,34 +1,45 @@
-from typing import Set, Iterable, Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
-from entity import Entity
-from game_map import GameMap
-from input_handlers import EventHandler
+from input_handlers import MainGameEventHandler
 
+if TYPE_CHECKING:
+    from entity import Actor
+    from game_map import GameMap
+    from input_handlers import EventHandler
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
-        self.event_handler = event_handler
-        self.game_map = game_map
+    game_map: GameMap
+    def __init__(self, player: Actor):
+        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
 
-    def handle_events(self, events: Iterable[Any]) -> None:
-        for event in events:
-            action = self.event_handler.dispatch(event)
+    def handle_enemy_turns(self) -> None:
+        for entity in set(self.game_map.actors) - {self.player}:
+            if entity.ai:
+                entity.ai.perform()
 
-            if action is None:
-                continue
-            
-            action.perform(self, self.player)
-
+    def update_fov(self) -> None:
+       """Recompute the visible area based on the players point of view."""
+       self.game_map.visible[:] = compute_fov(
+           self.game_map.tiles["transparent"],
+           (self.player.x, self.player.y),
+           radius=8,
+       )
+       # If a tile is "visible" it should be added to "explored".
+       self.game_map.explored |= self.game_map.visible
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
-        for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
-
+        console.print(
+            x=1,
+            y=47,
+            string=f"HP: {self.player.fighter.hp}/{self.player.fighter.max_hp}",
+        )
         context.present(console)
 
         console.clear()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+import copy
 import tcod
 from engine import Engine
-from entity import Entity
-from input_handlers import EventHandler
+import entity_factories
 from procgen import generate_dungeon
 def main() -> None:
     screen_width = 80
@@ -12,23 +12,24 @@ def main() -> None:
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
+    max_monsters_per_room = 2
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-    event_handler = EventHandler()
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
-    entities = {npc, player}
-    game_map = generate_dungeon(
+    player = copy.deepcopy(entity_factories.player)
+    engine = Engine(player=player)
+    
+    engine.game_map = generate_dungeon(
        max_rooms=max_rooms,
        room_min_size=room_min_size,
        room_max_size=room_max_size,
        map_width=map_width,
        map_height=map_height,
-       player=player
+       max_monsters_per_room=max_monsters_per_room,
+       engine=engine,
    )
-    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+    engine.update_fov()
     
     with tcod.context.new_terminal(
         screen_width,
@@ -40,9 +41,7 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             engine.render(console=root_console, context=context)
-
-            events = tcod.event.wait()
-            engine.handle_events(events)
+            engine.event_handler.handle_events()
 
 
 
