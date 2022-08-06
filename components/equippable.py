fa7b57b309e20a595271e4465019d56737bdc33f
Jedai Saboteur <paradox.inversion@gmail.com>
2022-08-06 13:48:33 -0700

    Update equippable

--- components/equippable.py
+++ components/equippable.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
-
 from typing import TYPE_CHECKING
-
 from components.base_component import BaseComponent
 from equipment_types import EquipmentType
 
 if TYPE_CHECKING:
     from entity import Item
+
 class Equippable(BaseComponent):
     parent: Item
 
@@ -17,7 +16,6 @@ class Equippable(BaseComponent):
         defense_bonus: int = 0,
     ):
         self.equipment_type = equipment_type
-
         self.power_bonus = power_bonus
         self.defense_bonus = defense_bonus
 
