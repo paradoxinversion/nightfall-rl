3958ea3720fca5b41adb477e9468e7dec368d638
Jedai Saboteur <paradox.inversion@gmail.com>
2022-08-06 13:49:07 -0700

    Cleanup equipment

--- components/equipment.py
+++ components/equipment.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
-
 from typing import Optional, TYPE_CHECKING
-
 from components.base_component import BaseComponent
 from equipment_types import EquipmentType
 
 if TYPE_CHECKING:
     from entity import Actor, Item
-
 
 class Equipment(BaseComponent):
     parent: Actor
