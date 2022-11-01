0732576389bc3c29b57853909986f221c6425887
Jedai Saboteur <paradox.inversion@gmail.com>
2022-10-31 20:14:03 -0700

    Added Deeds

---
+++ components/deeds.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from components.base_component import BaseComponent
+
+class Deeds(BaseComponent):
+    def __init__(self):
+        self.evil_entities_slain = 0
+        self.characters_murdered = 0
