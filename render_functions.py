3f6aa46c853fcdc9d851e0e5c67ff0daea168de6
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-05 19:23:26 -0800

    Made render_bar more generic

--- render_functions.py
+++ render_functions.py
@@ -20,20 +20,20 @@ def get_names_at_location(x: int, y: int, game_map: GameMap) -> str:
     return names.capitalize()
 
 def render_bar(
-    console: Console, current_value: int, maximum_value: int, total_width: int
+    console: Console, current_value: int, maximum_value: int, total_width: int, x: int, y: int, text: str
 ) -> None:
     """Render a bar such as for HP or MP"""
     bar_width = int(float(current_value) / maximum_value * total_width)
 
-    console.draw_rect(x=0, y=45, width=total_width, height=1, ch=1, bg=color.bar_empty)
+    console.draw_rect(x=x, y=y, width=total_width, height=1, ch=1, bg=color.bar_empty)
 
     if bar_width > 0:
         console.draw_rect(
-            x=0, y=45, width=bar_width, height=1, ch=1, bg=color.bar_filled
+            x=x, y=y, width=bar_width, height=1, ch=1, bg=color.bar_filled
         )
 
     console.print(
-        x=1, y=45, string=f"HP: {current_value}/{maximum_value}", fg=color.bar_text
+        x=x+1, y=y, string=f"{text}: {current_value}/{maximum_value}", fg=color.bar_text
     )
 
 def render_dungeon_level(
